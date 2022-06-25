59a226095bb4ecf7116df67e9a9417bc20c14e44
Alexander R. Torrijos <artorrijos@gmail.com>
2022-06-25 16:07:31 +0800

    Added simple webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+// src/index.js default configuration
+const path = require("path");
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+};
